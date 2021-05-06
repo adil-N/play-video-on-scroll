e32e892db4ccd07ab9343c6cced74fb2b4ce1729
adil-N <66251979+adil-N@users.noreply.github.com>
2021-05-06 14:16:44 +0000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+function scrollVideo() {
+    var video = $('#video').get(0),
+        videoLength = video.duration,
+        scrollPosition = $(document).scrollTop();
+
+    video.currentTime = (scrollPosition / ($(document).height() - $(window).height())) * videoLength;
+    
+}
+
+$(window).scroll(function(e) {
+    scrollVideo();
+});
+
+
+window.onload = function(){document.getElementsByTagName('html')[0].className = '';}
+
+
